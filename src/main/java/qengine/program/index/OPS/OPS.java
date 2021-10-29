8565e7a30c1645c69922339b3bf72cb91682eb03
Daemoryn <daemoryn@gmail.com>
2021-10-29 17:59:36 +0200

    Implementation OPS
    constructeur privée, triplet public, getInstance (singleton)

--- src/main/java/qengine/program/index/OPS/OPS.java
+++ src/main/java/qengine/program/index/OPS/OPS.java
@@ -1,5 +1,22 @@
 package qengine.program.index.OPS;
 
+import qengine.program.utils.Triplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OPS {
-    
+    private static OPS instance = null;
+    public List<Triplet> triplets;
+
+    private OPS() {
+        this.triplets = new ArrayList<>();
+    }
+
+    public static OPS getInstance() {
+        if (instance == null) {
+            instance = new OPS();
+        }
+        return instance;
+    }
 }
