fa0b160fa9152a9a2be00e6406c6295872e0ee90
benjamin <adolphe906@gmail.com>
2021-11-19 17:47:09 +0100

    update data Struct

--- src/main/java/qengine/program/index/MyIndex.java
+++ src/main/java/qengine/program/index/MyIndex.java
@@ -6,7 +6,7 @@ import qengine.program.timers.Timers;
 import java.util.*;
 
 public abstract class MyIndex {
-    public Map<Integer, List<Map<Integer, Integer>>> tree;
+    public Map<Integer, Map<Integer, List<Integer>>> tree;
     protected Dictionary dictionary = Dictionary.getInstance();
     protected static final Timers TIMERS = Timers.getInstance();
 
@@ -18,28 +18,28 @@ public abstract class MyIndex {
 
     //  Ajout des indices à l'arbre
     protected void addStatementToIndex(Integer a, Integer b, Integer c) {
-        HashMap<Integer, Integer> intermediaire = new HashMap<>();
-        intermediaire.put(b, c);
-        addElementToMap(a, tree, intermediaire);
-    }
-
-    protected Map<Integer, List<Map<Integer, Integer>>> addElementToMap(Integer a, Map<Integer, List<Map<Integer, Integer>>> map,Map<Integer, Integer> intermediaire) {
-        if (map.containsKey(a)) {
-            map.get(a).add(intermediaire);
+        if (tree.containsKey(a)) {
+            if (tree.get(a).containsKey(b)) {
+                tree.get(a).get(b).add(c);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(c);
+                tree.get(a).put(b, list);
+            }
         } else {
-            List<Map<Integer, Integer>> intermediairelist = new ArrayList<>();
-            intermediairelist.add(intermediaire);
-            map.put(a, intermediairelist);
+            HashMap<Integer, List<Integer>> intermediaire = new HashMap<>();
+            List<Integer> list2 = new ArrayList<>();
+            list2.add(c);
+            intermediaire.put(b, list2);
+            tree.put(a, intermediaire);
         }
-        return map;
     }
 
-    protected List<Integer> getResGeneral(Integer i1,Integer i2){
+    protected List<Integer> getResGeneral(Integer i1, Integer i2) {
         List<Integer> res = new ArrayList<>();
-        List<Map<Integer, Integer>> pairPredSubList = tree.get(i1);
-        for (Map<Integer, Integer> pairPredSubject : pairPredSubList) {
-            if (!pairPredSubject.containsKey(i2)) continue;
-            res.add(pairPredSubject.get(i2));
+        for (Map.Entry<Integer, List<Integer>> pairPredSubject : tree.get(i1).entrySet()) {
+            if (!pairPredSubject.getKey().equals(i2)) continue;
+            res.addAll(pairPredSubject.getValue());
         }
         return res;
     }
@@ -59,13 +59,11 @@ public abstract class MyIndex {
     @Override
     public String toString() {
         StringBuilder st = new StringBuilder();
-        for (Map.Entry<Integer, List<Map<Integer, Integer>>> entry : tree.entrySet()) {
-            for (Map<Integer, Integer> integerIntegerMap : entry.getValue()) {
-                for (Map.Entry<Integer, Integer> entry1 : integerIntegerMap.entrySet()) {
-                    st.append("<").append(entry.getKey()).append(",").append(entry1.getKey()).append(",").append(entry1.getValue()).append(">");
-                    if (!entry1.getKey().equals(integerIntegerMap.entrySet().toArray()[integerIntegerMap.entrySet().size() - 1])) {
-                        st.append("\n");
-                    }
+        for (Map.Entry<Integer, Map<Integer, List<Integer>>> entry : tree.entrySet()) {
+            for(Map.Entry<Integer,List<Integer>> entry1 : entry.getValue().entrySet()){
+                for (Integer integer : entry1.getValue()) {
+                    st.append("<").append(entry.getKey()).append(",").append(entry1.getKey()).append(",").append(integer).append(">\n");
+
                 }
             }
         }
